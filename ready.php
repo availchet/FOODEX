df76c62948e3da5b715dbd05ed1e6b31fcb6d642
Chetanya Kandhari <availchet@users.noreply.github.com>
2016-04-26 00:03:15 +0530

    Mega Commit

---
+++ ready.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ready</title>
+		<style>
+			body
+			{
+				background: url("images/high-quality-food-wallpapers-8-background-wallpaper.jpg");
+			}
+		</style>
+	</head>
+	<body style="color:#808080;">
+		&nbsp;&nbsp;&nbsp;CONFIRMATION SUCCESSFUL!!<BR />
+		<br />
+		ORDER NUMBER IS:#12;<br /><br />
+		IT WILL BE REAGY IN 15 MINUTES.
+	</body>
+</html>
